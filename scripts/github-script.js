6e3cb3e37766616a3e253ea1e3df4e7921837164
Nicholas Bucher <behappy54321@gmail.com>
2024-11-12 11:59:08 -0500

    github script .
    Signed-off-by: Nicholas Bucher <behappy54321@gmail.com>

--- scripts/github-script.js
+++ scripts/github-script.js
@@ -1,11 +1,21 @@
-/**
- * Github Controls
- * Tampermonkey script.
- *
- */
+// ==UserScript==
+// @name         Github Controls
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Nick Bucher
+// @match        https://github.com/*/issues/*
+// @match        https://github.com/*/pull/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
 
-async function main() {
-  console.log("okay! In main!");
-}
+(function () {
+  "use strict";
 
-main();
+  async function main() {
+    console.log("[TM] Loaded Tampermonkey Script! 'Github Controls'");
+  }
+
+  main();
+})();
